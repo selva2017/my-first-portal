1159e05cbd791a04786856d3a2ba1d0521432694
Selvaraj Arumuthu <Se9h4h2@gap.com>
2017-06-11 07:14:37 -0400

    Template forms added to the shopping list module

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
 ingredientsChanged = new Subject<Ingredient[]>();
+startedEditing = new Subject<number>();
 // ingredientsChanged = new EventEmitter<Ingredient[]>();
  private ingredients: Ingredient[] = [
     new Ingredient('Apples',5),
@@ -12,6 +13,10 @@ ingredientsChanged = new Subject<Ingredient[]>();
 
     getIngredients(){
         return this.ingredients.slice();
+    }
+
+    getingredient(index: number){
+        return this.ingredients[index];
     }
 
     addIngredient(ingredient: Ingredient) {
@@ -27,5 +32,15 @@ ingredientsChanged = new Subject<Ingredient[]>();
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.next(this.ingredients.slice());
         // this.ingredientsChanged.emit(this.ingredients.slice());
-        }
+    }
+    
+    updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number){
+        this.ingredients.splice(index,1);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 }
