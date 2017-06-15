0fdbd921481476767e774c94fc742e2d637ee908
Selvaraj Arumuthu <Se9h4h2@gap.com>
2017-06-15 19:47:58 -0400

    http get and fetch the data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataStorageService {
     constructor(private http: Http, private recipeService: RecipeService){
     }
     
-    storeRecipes(){
+    storeRecipes() {
         return this.http.put('https://ng-recipe-book-fb935.firebaseio.com/recipes.json', 
         this.recipeService.getRecipes());
+    }
+
+    getRecipes() {
+        this.http.get('https://ng-recipe-book-fb935.firebaseio.com/recipes.json')
+        .map(
+            (response: Response) => {
+                const recipes: Recipe[] = response.json();
+                for(let recipe of recipes){
+                    if (!recipe['ingredients']){
+                        recipe['ingrediens'] = [];
+                    }
+                }
+                return recipes;
+            }
+        )
+        .subscribe(
+            (recipes: Recipe[]) => {
+                this.recipeService.setRecipes(recipes);
+            }
+        );
     }
 }
